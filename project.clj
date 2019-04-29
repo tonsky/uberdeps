1034ea9f7d6bfa11f2b5fd634f3306654fbfbaec
Nikita Prokopov <prokopov@gmail.com>
2019-04-29 21:34:20 +0300

    Set description, license and url in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject uberdeps "0.1.0"
+  :description "Uberjar builder for deps.edn"
+  :license     {:name "MIT" :url "https://github.com/tonsky/uberdeps/blob/master/LICENSE"}
+  :url         "https://github.com/tonsky/uberdeps"
+
   :dependencies
   [[org.clojure/clojure "1.10.1-beta2"]
-   [org.clojure/tools.deps.alpha "0.6.496"]]
-  :main uberdeps.main)
+   [org.clojure/tools.deps.alpha "0.6.496"]])
